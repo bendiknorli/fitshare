147f2f70f4fde4acf83a5bd9d4d4d3e52ee6fe3b
bjornhom <bjornhom@ntnu.no>
2023-03-09 15:57:57 +0100

    fix: deleted member-file

--- fitshare/src/components/Member.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export function Member(props: { name: string }) {
-  return <div className="Friend">
-    <div className="Friend-profile-pic"></div>
-    <div className="Friend-name">{props.name}</div>
-  </div>;
-}
